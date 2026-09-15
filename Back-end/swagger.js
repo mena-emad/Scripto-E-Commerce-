fc1dd6f2177b6bf362a1cf2ee8b75fcbbaed3ff6
mena-emad <menaemad6543@gmail.com>
2026-09-15 19:25:29 +0300

    final touches

--- Back-end/swagger.js
+++ Back-end/swagger.js
@@ -1,5 +1,17 @@
 import swaggerUi from "swagger-ui-express";
 import yaml from "yaml";
 import fs from "fs";
-const swaggerDocument = yaml.parse(fs.readFileSync("./swagger.yaml", "utf-8"));
-export  {swaggerUi , swaggerDocument};
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const swaggerDocument = yaml.parse(
+  fs.readFileSync(
+    path.join(__dirname, "swagger.yaml"),
+    "utf-8"
+  )
+);
+
+export { swaggerUi, swaggerDocument };
